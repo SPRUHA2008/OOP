ffacdb0b23c5fda3bd67b1e622e3fa7292ad7955
SPRUHA2008 <kurlespruha@gmail.com>
2025-09-09 10:25:05 +0530

    Update Q1.cpp

--- exp 5/Q1.cpp
+++ exp 5/Q1.cpp
@@ -39,3 +39,64 @@ int main() {
     return 0;
 }
 
+Q1.2]
+#include <iostream>
+using namespace std;
+
+class Sum {
+    int n, total;
+
+public:
+    Sum(int value) {
+        n = value;
+        total = 0;
+        for (int i = 1; i <= n; i++) {
+            total += i;
+        }
+    }
+
+    void display() {
+        cout << "Sum of numbers from 1 to " << n << " is: " << total << endl;
+    }
+};
+
+int main() {
+    int number;
+    cout << "Enter the value of N: ";
+    cin >> number;
+
+    Sum obj(number); 
+    obj.display();   
+
+    return 0;
+}
+Q1.1]
+    #include <iostream>
+using namespace std;
+
+class Sum {
+    int n, sum;
+
+public:
+    Sum() {
+        cout << "Enter the value of N: ";
+        cin >> n;
+
+        sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+    }
+
+    void display() {
+        cout << "Sum of numbers from 1 to " << n << " is: " << sum << endl;
+    }
+};
+
+int main() {
+    Sum obj;        
+    obj.display();  
+
+    return 0;
+}
+
